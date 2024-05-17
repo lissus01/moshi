fbf63befce1a901334d1be95cb3a8083ac5ad94d
Francisca <fr.ramirezs@duocuc.cl>
2024-05-17 13:27:19 -0400

    actualizacion final2

--- carrito.js
+++ carrito.js
@@ -1,38 +1,57 @@
-document.addEventListener("DOMContentLoaded",function(){
+document.addEventListener("DOMContentLoaded", function () {
     const añadirCarrito = document.querySelectorAll(".añadir-carrito");
     const listaCarrito = document.querySelector(".carrito-items");
     const carritoTotal = document.getElementById("carrito-total-id");
     const abrirCarrito = document.getElementById("carrito-boton-id");
     const carritoPanel = document.getElementById("carrito-panel-id");
     const cerrarCarrito = document.getElementById("carrito-cerrar-id");
-    
-    let carritoTotalValor = 0;
+
+    let carritoItems = JSON.parse(localStorage.getItem("carritoItems")) || [];
+    let carritoTotalValor = JSON.parse(localStorage.getItem("carritoTotalValor")) || 0;
+
+    const saveLocal = () => {
+        localStorage.setItem("carritoItems", JSON.stringify(carritoItems));
+        localStorage.setItem("carritoTotalValor", JSON.stringify(carritoTotalValor));
+    };
+
+    const renderCartItems = () => {
+        listaCarrito.innerHTML = ""; 
+        carritoItems.forEach(item => {
+            const carritoItem = document.createElement("li");
+            carritoItem.classList.add("carrito-item");
+            carritoItem.textContent = `${item.nombre} - $${item.precio}`;
+            listaCarrito.appendChild(carritoItem);
+        });
+        carritoTotal.textContent = carritoTotalValor;
+        carritoPanel.style.display = carritoItems.length > 0 ? "block" : "none"; 
+    };
+
+    renderCartItems();
 
     añadirCarrito.forEach(button => {
-        button.addEventListener("click" , function(){
+        button.addEventListener("click", function () {
             const nombreProducto = button.getAttribute("data-name");
             const precioProducto = parseInt(button.getAttribute("data-price"));
 
-            const carritoItem = document.createElement("li");
-            carritoItem.classList.add("carrito-item");
-            carritoItem.textContent = `${nombreProducto} - $${precioProducto}`;
-
-            listaCarrito.appendChild(carritoItem);
+            carritoItems.push({ nombre: nombreProducto, precio: precioProducto });
 
             carritoTotalValor += precioProducto;
-            carritoTotal.textContent = carritoTotalValor;
 
-            carritoPanel.style.display = "block";
+            saveLocal();
+            renderCartItems();
         });
     });
 
-    abrirCarrito.addEventListener("click" , function(){
+    abrirCarrito.addEventListener("click", function () {
         carritoPanel.classList.toggle("active");
     });
 
     cerrarCarrito.addEventListener("click", function(){
         carritoPanel.style.display = "";
         carritoPanel.classList.remove("active");
-        
+        saveLocal();
     });
 });
+
+
+
